cacd022f96aa35bcf7b2b153690877b2d85e5563
AadityaTailor <you@example.com>
2021-04-07 21:53:03 +0530

    Updates

---
+++ precache-manifest.fe11fe71ee8293f03277e6cba4c44357.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "250dddeb0dec7e6690feea6ef28fc47e",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "726f0ccba0f37b18ab36",
+    "url": "/todo/static/css/main.5b68f0d0.chunk.css"
+  },
+  {
+    "revision": "99a252dddb7d4619436f",
+    "url": "/todo/static/js/2.4f46ce4b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.4f46ce4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "726f0ccba0f37b18ab36",
+    "url": "/todo/static/js/main.2c204e56.chunk.js"
+  },
+  {
+    "revision": "b013342699353324aadc",
+    "url": "/todo/static/js/runtime-main.37b3b10e.js"
+  }
+]);
